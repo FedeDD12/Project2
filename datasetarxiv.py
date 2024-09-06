e936c485af8834f8770234523c2c585fd8784d6f
FedeDD12 <federica.delduchetto@studio.unibo.it>
2024-09-06 22:51:27 +0200

    Update datasetarxiv.py

--- datasetarxiv.py
+++ datasetarxiv.py
@@ -1,4 +1,5 @@
 import pandas as pd 
 
-dataset=pd.read_json("math_arXiv_v0.2_chunk_4.jsonl", lines=True)
-dataset.to_csv("math_arXiv_v0.2_chunk_4.csv")
+
+dataset=dataset.read_csv("math_arXiv_v0.2_chunk_4.csv")
+print(dataset)
